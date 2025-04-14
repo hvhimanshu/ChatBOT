7ace9d47fc37246baacb94791442e5e1723e8f4b
Himanshu Vishwakarma <85735250+hvhimanshu@users.noreply.github.com>
2025-04-14 13:43:01 +0530

    Updated port setting to use gunicorn

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 import io
+import os
 
 app = Flask(__name__)
 
@@ -36,4 +37,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(debug=False,host = '0.0.0.0',port=port)
